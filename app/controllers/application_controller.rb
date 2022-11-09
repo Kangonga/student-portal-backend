bbb271bdaea5568fc6ad9874154d73bbcf8af87b
Shanks <onyangogeorge954@gmail.com>
2022-11-09 11:04:25 +0300

    Add new faker gem and house keeping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,5 +24,27 @@ class ApplicationController < Sinatra::Base
     unit.to_json
   end
 
+    #Student Section
+  # Create a new Student
+  post "/students" do
+    student = Student.create(name: params[:name], surname: params[:surname], gender: params[:gender], password: params[:password], national_id: params[:national_id], admission_number: params[:admission_number], date_of_birth: params[:date_of_birth])
+    student.to_json
+  end
+
+  patch "/students/:id" do
+    student = Student.find(params[:id])
+    student.update(name: params[:name], surname: params[:surname], password: params[:password], national_id: params[:national_id], admission_number: params[:admission_number], date_of_birth: params[:date_of_birth])
+  end
+
+  get "/students/:id" do
+    student = Student.find(params[:id])
+    student.to_json
+  end
+
+  get "/students" do
+    students = Student.all
+    students.to_json
+  end
+
  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,24 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+# Unit.create(name: "Unit 1", description: "Unit 1 is about...", code: 1, unit_grade: "A", category_id: 1)
+# Unit.create(name: "Unit 2", description: "Unit 2 is about...", code: 2, unit_grade: "B", category_id: 1)
+# Unit.create(name: "Unit 3", description: "Unit 3 is about...", code: 3, unit_grade: "C", category_id: 1)
+# Unit.create(name: "Unit 4", description: "Unit 4 is about...", code: 4, unit_grade: "D", category_id: 1)
+
+# Student.create(name: "John", surname: "Doe", gender: "male", password: "1234", national_id: 12345678, admission_number: 123456, date_of_birth: "1990-01-01")
+# Student.create(name: "Jane", surname:"Mary", gender: "female", password: "1234", national_id: 12345678, admission_number: 123456, date_of_birth: "1990-01-01")
+# Student.create(name: "James", surname: "koga", gender: "male", password: "1234", national_id: 12345678, admission_number: 123456, date_of_birth: "1990-01-01")
+
+Student.all.each do |student|
+    3.times do
+        Unit.create(name: Faker::Educator.course, description: Faker::Lorem.paragraph, code: Faker::Number.number(digits: 4), unit_grade: Faker::Number.between(from: 1, to: 10), category_id: student.id)
+    end
+end
+    
+
+# puts "#{Student.count} students created"
+# puts "#{Unit.count} units created"
+puts Student.all.to_json
+
 puts "✅ Done seeding!"
